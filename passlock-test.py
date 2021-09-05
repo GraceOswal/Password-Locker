6bc245ca6b1880afd49b9ab29e9e9fb6f2a047a8
GraceOswal <graceoswal88@gmail.com>
2021-09-05 05:22:55 -0400

    add user test

---
+++ passlock-test.py
@@ -0,0 +1,27 @@
+mport unittest #Importing the unittest module
+from passlock import User  #this imports the user class
+from passlock import Credentials  #this imports the credential class
+
+class TestClass(unittest.TestCase):
+    """
+    This test defines the test cases for the behaviour of user classself.
+    """
+    
+ def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User("Gracegee","grcee") # create user object
+
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.username,"Gracegee")
+        self.assertEqual(self.new_user.password,"grcee")
+
+
+if __name__ == '__main__':
+    unittest.main()
