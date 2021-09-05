01847028ee8524cf368a6c1b3655173ace56f202
GraceOswal <graceoswal88@gmail.com>
2021-09-05 15:30:14 -0400

    add save new user

--- passlock_test.py
+++ passlock_test.py
@@ -21,5 +21,13 @@ class TestClass(unittest.TestCase):
         self.assertEqual(self.new_user.username,"Gracegee")
         self.assertEqual(self.new_user.password, "grce")
         
+    def test_save_user(self):
+        '''
+        test_save_user test case to test if the user object is saved into the
+        user list
+        '''
+        self.new_user.save_user() #this saves new user
+        self.assertEqual(len(User.user_list),1)
+        
 if __name__ == '__main__':
     unittest.main()
